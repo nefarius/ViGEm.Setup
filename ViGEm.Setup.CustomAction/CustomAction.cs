15605f67b095fa24902e868c0378e555e5975d60
Benjamin Höglinger-Stelzer <benjamin.hoeglinger@nefarius.at>
2019-04-13 01:16:49 +0200

    Added debug section

--- ViGEm.Setup.CustomAction/CustomAction.cs
+++ ViGEm.Setup.CustomAction/CustomAction.cs
@@ -1,7 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
 using Microsoft.Deployment.WindowsInstaller;
+using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using ViGEm.Setup.CustomAction.Util;
 
 namespace ViGEm.Setup.CustomAction
 {
@@ -11,6 +13,19 @@ namespace ViGEm.Setup.CustomAction
         public static ActionResult CustomAction1(Session session)
         {
             session.Log("Begin CustomAction1");
+
+            var busGuid = Guid.Parse("{96E42B22-F5E9-42F8-B043-ED0F932F014F}");
+
+            var index = 0;
+
+            while (Devcon.FindDeviceByInterfaceId(busGuid, out var path, out var instanceId, index))
+            {
+                MessageBox.Show($"{path} - {instanceId}");
+
+                index++;
+            }
+
+            MessageBox.Show(new Win32Exception(Marshal.GetLastWin32Error()).Message);
 
             return ActionResult.Success;
         }
