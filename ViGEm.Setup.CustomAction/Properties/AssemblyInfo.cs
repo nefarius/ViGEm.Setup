77fe484877cb5b953843f0ee87dfdfbec27e5963
Benjamin Höglinger-Stelzer <benjamin.hoeglinger@nefarius.at>
2019-04-15 12:57:06 +0200

    Updated AssemblyInfo.cs

--- ViGEm.Setup.CustomAction/Properties/AssemblyInfo.cs
+++ ViGEm.Setup.CustomAction/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ViGEm.Setup.CustomAction")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("WiX Toolset Custom Actions for driver management")]
+[assembly: AssemblyCompany("Nefarius Software Solutions e.U.")]
 [assembly: AssemblyProduct("ViGEm.Setup.CustomAction")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © Nefarius Software Solutions e.U. 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
